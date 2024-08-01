1a5e5b7d823a8c0a80ea94237e931b3d24d76ee9
Genzi135 <phu13052002@gmail.com>
2024-08-01 16:25:24 +0700

    change team card

--- src/components/TeamCard.jsx
+++ src/components/TeamCard.jsx
@@ -3,10 +3,10 @@ import Image from "next/image";
 export default function TeamCard({ props }) {
     return (
         <div className="flex flex-col justify-center items-center gap-2 w-80 h-96 p-5 bg-white rounded-lg shadow-lg">
-            <div className="flex justify-center items-center rounded-full bg-gray-100">
+            {/* <div className="flex justify-center items-center rounded-full bg-gray-100">
                 <Image src={props.image} alt="image" width={150} height={150} />
 
-            </div>
+            </div> */}
             <div className="flex justify-center items-center">
                 <label className="text-lg font-manropeBold">{props.name}</label>
             </div>
