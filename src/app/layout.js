efbc0fffeb1d9284b768709520afc2481d04677b
Genzi135 <phu13052002@gmail.com>
2024-08-12 22:18:07 +0700

    add gg analisys

--- src/app/layout.js
+++ src/app/layout.js
@@ -24,6 +24,20 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        {/* Google Analytics */}
+        <script async src={`https://www.googletagmanager.com/gtag/js?id=G-K7903M6JL3`}></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-K7903M6JL3');
+            `,
+          }}
+        />
+      </head>
       <body className={inter.className + "font-manrope scroll-smooth text-black bg-white"}>
         <Navbar />
         <Toaster />
