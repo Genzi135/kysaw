070ff9f93a45176f6d7e7b190b63a708754beafa
Genzi135 <phu13052002@gmail.com>
2024-08-30 00:48:24 +0700

    add thong bao

--- src/app/(pages)/notification/page.jsx
+++ src/app/(pages)/notification/page.jsx
@@ -1,17 +1,17 @@
 import Image from "next/image";
 import { COLOR } from "../../../utils/COLORS";
 import { imgShared } from "../../../utils/images";
-import NewsCard from '../../../components/NewsCard';
+import ThongBaoCEO from '../../../../assets/notification/Thong-bao-CEO.jpg'
 
 export default function News() {
-    const data = [
-        {
-            type: "Thông báo",
-            title: "Về việc thay đổi đại diện pháp nhân và CEO của Công ty",
-            time: "29/08/2024",
-            pdf: "https://kysaw.s3.ap-southeast-1.amazonaws.com/Thong+Bao+CEO.pdf"
-        },
-    ]
+    // const data = [
+    //     {
+    //         type: "Thông báo",
+    //         title: "Về việc thay đổi đại diện pháp nhân và CEO của Công ty",
+    //         time: "29/08/2024",
+    //         pdf: "https://kysaw.s3.ap-southeast-1.amazonaws.com/Thong+Bao+CEO.pdf"
+    //     },
+    // ]
     return (
         <div className="mt-[70px] scroll-smooth">
             <div id="about" className="flex justify-center items-center" style={{ backgroundImage: `linear-gradient(to top, ${COLOR.backgroundPrimary},#15BEF2)` }}>
@@ -19,12 +19,8 @@ export default function News() {
             </div>
             <div className="flex flex-col justify-center items-center w-full h-auto">
                 <div className="w-full max-w-[1220px] flex flex-col justify-center items-center mb-10">
-                    <label className="text-2xl font-manropeBold md:text-4xl p-10">Tin Tức</label>
-                    {data.map((e, index) => {
-                        return (
-                            <NewsCard data={e} key={index} />
-                        )
-                    })}
+                    <label className="text-2xl font-manropeBold md:text-4xl p-10">Thông báo</label>
+                    <Image src={ThongBaoCEO} width={900} alt="Thong bao" />
                 </div>
             </div>
         </div>
--- src/components/PDFrender.jsx
+++
@@ -1,5 +0,0 @@
-export default function PDFrender({ src }) {
-    return (
-        <iframe src={src} width="100%" height="600px"></iframe>
-    )
-}
